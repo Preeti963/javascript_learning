18aa467aad85b808f013157ccd3b64776173c768
“Preeti <preetimagar963@gmail.com>
2025-12-27 21:55:32 +0545

    Day 2: Functions, if-else, do-while loop

---
+++ dowhile.js
@@ -0,0 +1,19 @@
+// What is a loop 
+// A lop lets us to run the same block of code again and again 
+// until a cetain  condition
+
+//Types of loop
+//1. for loop
+for(let i =1; i <=5; i++){
+    console.log("Clap number", i)
+}
+// for -
+for(let i =5; i >1; i--){
+    console.log("Clap number", i)
+}
+// let i = 1 ( start form 1)
+// i<=5 ( run as long as i is less then or equal to 5)
+// i++ increase i by 1 each time
+
+//2. while loop
+//3. do while loops
