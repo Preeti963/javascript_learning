240d9d48936869ddf5883edf36b076ef09710c6a
“Preeti <preetimagar963@gmail.com>
2026-01-04 21:06:33 +0545

    Day 10: hoisting and date object

---
+++ advance_javascript/date-object.js
@@ -0,0 +1,10 @@
+// creating a Date
+//  const now = new Date()
+//  console.log(now)
+//  to know its  type(typeof now) and 
+//for present date  new Date()
+
+const now = new Date();
+
+ const birthday = new Date("2025-02-07");
+ console.log(birthday);
